cfbc3ac3fedd5c851999856d29a1e5c55fcab812
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-02-12 04:28:25 +0300

    first commit

---
+++ scissor.js
@@ -0,0 +1,72 @@
+const choices = document.querySelectorAll('.choice');
+const playerScoreElem = document.querySelector('.player-score');
+const computerScoreElem = document.querySelector('.computer-score');
+const resultElem = document.querySelector('#result');
+const computerChoiceElem = document.querySelector('#computer-choice');
+
+const weapons = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+
+// Function to generate random weapon for computer
+function computerPlay() {
+  const weaponIndex = Math.floor(Math.random() * weapons.length);
+  return weapons[weaponIndex];
+}
+
+// Function to update score and display result
+function updateScore(playerWeapon, computerWeapon) {
+  if (playerWeapon) {
+    computerChoiceElem.innerHTML = `Computer choose: ${computerWeapon}.`;
+    if (playerWeapon === computerWeapon) {
+      resultElem.innerHTML = "It's a tie!";
+    } else if (
+      (playerWeapon === 'rock' && computerWeapon === 'scissors') ||
+      (playerWeapon === 'paper' && computerWeapon === 'rock') ||
+      (playerWeapon === 'scissors' && computerWeapon === 'paper')
+    ) {
+      resultElem.innerHTML = 'You win!';
+      playerScore++;
+      playerScoreElem.innerHTML = `Player: ${playerScore}`;
+    } else {
+      resultElem.innerHTML = 'Computer wins!';
+      computerScore++;
+      computerScoreElem.innerHTML = `Computer: ${computerScore}`;
+    }
+  } else {
+    computerChoiceElem.innerHTML = `Game Over`;
+    resultElem.innerHTML = 'You did not make a choice! | You lose the game!';
+    resultElem.style.color = 'red';
+    disableOptions();
+  }
+
+  if (playerScore === 5) {
+    resultElem.textContent = 'You win the game!';
+    resultElem.style.color = 'green';
+    computerChoiceElem.innerHTML = 'Game Over';
+    disableOptions();
+  }
+
+  if (computerScore === 5) {
+    resultElem.textContent = 'You lose the game!';
+    resultElem.style.color = 'red';
+    computerChoiceElem.innerHTML = 'Game Over';
+    disableOptions();
+  }
+}
+
+// Function to handle player choice
+function selectWeapon() {
+  const playerWeapon = this.id;
+  const computerWeapon = computerPlay();
+  updateScore(playerWeapon, computerWeapon);
+}
+
+function disableOptions() {
+  choices.forEach((choice) => {
+    choice.style.pointerEvents = 'none';
+  });
+}
+
+// Event listeners
+choices.forEach((choice) => choice.addEventListener('click', selectWeapon));
